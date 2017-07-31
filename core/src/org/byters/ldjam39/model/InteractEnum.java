7f292a07997592d5ed06667e1da216c13e3369d5
blackdizel <blackdizel@gmail.com>
2017-07-31 19:10:03 +0300

    add items enum and interact enum

---
+++ core/src/org/byters/ldjam39/model/InteractEnum.java
@@ -0,0 +1,9 @@
+package org.byters.ldjam39.model;
+
+public enum InteractEnum {
+    GET_HAMMER,
+    GET_MONEY,
+    GO_TO_OUTDOOR,
+    GET_FISHING_ROD,
+    FILL_CAT_DISH
+}
---
+++ core/src/org/byters/ldjam39/model/WorldItemsEnum.java
@@ -0,0 +1,11 @@
+package org.byters.ldjam39.model;
+
+public enum WorldItemsEnum {
+    HAMMER,
+    FISHING_ROD,
+    CAT_DISH_EMPTY,
+    CAT_DISH_FULL,
+    INVENTORY_CAT_FOOD,
+    MONEY,
+    HOME_DOOR
+}
