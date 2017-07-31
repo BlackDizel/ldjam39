d034fe8868cfde53f394c3fa50fa7b45ae1f765d
blackdizel <blackdizel@gmail.com>
2017-07-31 19:11:05 +0300

    set interacted object item and action

--- core/src/org/byters/ldjam39/model/InteractedObject.java
+++ core/src/org/byters/ldjam39/model/InteractedObject.java
@@ -3,17 +3,32 @@ package org.byters.ldjam39.model;
 public class InteractedObject {
     private final String message;
     private final DrawableObject drawableObject;
+    private InteractEnum action;
+    private WorldItemsEnum itemWorld;
 
-    public InteractedObject(String message, DrawableObject drawableObject) {
+    InteractedObject(InteractEnum idInteract,
+                            WorldItemsEnum itemWorld,
+                            String message,
+                            DrawableObject drawableObject) {
         this.message = message;
+        this.itemWorld = itemWorld;
         this.drawableObject = drawableObject;
+        this.action = idInteract;
     }
 
-    public String getMessage() {
+    String getMessage() {
         return message;
     }
 
-    public DrawableObject getDrawableObject() {
+    DrawableObject getDrawableObject() {
         return drawableObject;
+    }
+
+    public InteractEnum getAction() {
+        return action;
+    }
+
+    WorldItemsEnum getWorldItem() {
+        return itemWorld;
     }
 }
