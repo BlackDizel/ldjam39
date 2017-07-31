cec329af438c8c4c449a88d8a394a7de2125dd7d
blackdizel <blackdizel@gmail.com>
2017-07-31 16:14:30 +0300

    add string enum

---
+++ core/src/org/byters/ldjam39/model/StringEnum.java
@@ -0,0 +1,22 @@
+package org.byters.ldjam39.model;
+
+public enum StringEnum {
+    //todo put into json
+    GET_HAMMER("get hammer from locker"),
+    GET_MONEY("get money from store"),
+    GO_TO_OUTDOOR("go to outdoor"),
+    GET_FISHING_ROD("get fishing rod"),
+    FILL_CAT_DISH("fill cat dish"),
+    PRESS_E("Press E to ");
+
+    private String title;
+
+    StringEnum(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
