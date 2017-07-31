f02b4d28df49ba29737c22026a224bbe11750761
blackdizel <blackdizel@gmail.com>
2017-07-31 19:14:36 +0300

    add outdoor screen stub

---
+++ core/src/org/byters/ldjam39/view/ScreenOutdoor.java
@@ -0,0 +1,11 @@
+package org.byters.ldjam39.view;
+
+import org.byters.ldjam39.model.LocationInfoBase;
+
+public class ScreenOutdoor extends ScreenGameBase {
+    @Override
+    LocationInfoBase getLocationInfo() {
+        //todo implement
+        return null;
+    }
+}
