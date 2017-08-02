458fdbfdd436ba8dc14e4a7399d9ccbdeb8a4a88
blackdizel <blackdizel@gmail.com>
2017-08-02 20:53:54 +0300

    add success and fail messages to interacted object

--- core/src/org/byters/ldjam39/model/InteractedObject.java
+++ core/src/org/byters/ldjam39/model/InteractedObject.java
@@ -1,19 +1,27 @@
 package org.byters.ldjam39.model;
 
+import java.util.ArrayList;
+
 public class InteractedObject {
     private final String message;
     private final DrawableObject drawableObject;
     private InteractEnum action;
     private WorldItemsEnum itemWorld;
+    private ArrayList<String> listMessageSuccess;
+    private ArrayList<String> listMessageFail;
 
     public InteractedObject(InteractEnum idInteract,
                             WorldItemsEnum itemWorld,
                             String message,
-                            DrawableObject drawableObject) {
+                            DrawableObject drawableObject,
+                            ArrayList<String> listMessageSuccess,
+                            ArrayList<String> listMessageFail) {
         this.message = message;
         this.itemWorld = itemWorld;
         this.drawableObject = drawableObject;
         this.action = idInteract;
+        this.listMessageSuccess = listMessageSuccess;
+        this.listMessageFail = listMessageFail;
     }
 
     public String getMessage() {
@@ -30,5 +38,13 @@ public class InteractedObject {
 
     public WorldItemsEnum getWorldItem() {
         return itemWorld;
+    }
+
+    public ArrayList<String> getMessageSuccess() {
+        return listMessageSuccess;
+    }
+
+    public ArrayList<String> getMessageFault() {
+        return listMessageFail;
     }
 }
