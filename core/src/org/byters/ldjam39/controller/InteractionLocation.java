a8045c81a21c3e17cf1452820500e37262ef132e
blackdizel <blackdizel@gmail.com>
2017-08-02 20:53:22 +0300

    set message on interaction result

--- core/src/org/byters/ldjam39/controller/InteractionLocation.java
+++ core/src/org/byters/ldjam39/controller/InteractionLocation.java
@@ -6,88 +6,100 @@ import org.byters.ldjam39.model.WorldItemsEnum;
 import org.byters.ldjam39.model.locationInfo.LocationInfoBase;
 import org.byters.ldjam39.view.*;
 
-import java.lang.ref.WeakReference;
-
 public class InteractionLocation {
 
-    private WeakReference<LocationInfoBase> wLocationInfo;
+    private LocationInfoBase wLocationInfo;
 
     public InteractionLocation(LocationInfoBase locationInfo) {
-        wLocationInfo = new WeakReference<LocationInfoBase>(locationInfo);
+        wLocationInfo = locationInfo;
     }
 
     public void checkInteraction() {
-        if (wLocationInfo.get() == null) return;
+        if (wLocationInfo == null) return;
 
-        InteractedObject itemInteracted = wLocationInfo.get().getInteractedObject();
+        InteractedObject itemInteracted = wLocationInfo.getInteractedObject();
         if (itemInteracted == null) return;
 
+        boolean success = false;
+        boolean needToShow = true;
         switch (itemInteracted.getAction()) {
             case GET_HAMMER:
-                ControllerWorldState.getInstance().addToInventory(WorldItemsEnum.HAMMER);
+                success = ControllerWorldState.getInstance().addToInventory(WorldItemsEnum.HAMMER);
                 break;
             case GET_MONEY:
-                ControllerWorldState.getInstance().addToInventory(WorldItemsEnum.MONEY);
+                success = ControllerWorldState.getInstance().addToInventory(WorldItemsEnum.MONEY);
                 break;
             case GET_FISHING_ROD:
-                ControllerWorldState.getInstance().addToInventory(WorldItemsEnum.FISHING_ROD);
+                success = ControllerWorldState.getInstance().addToInventory(WorldItemsEnum.FISHING_ROD);
                 break;
             case GET_SHOVEL:
-                ControllerWorldState.getInstance().addToInventory(WorldItemsEnum.SHOVEL);
+                success = ControllerWorldState.getInstance().addToInventory(WorldItemsEnum.SHOVEL);
                 break;
 
             case GET_TREE:
-                ControllerWorldState.getInstance().getTree();
+                success = ControllerWorldState.getInstance().getTree();
                 break;
 
             case PLANT_TREE:
-                ControllerWorldState.getInstance().plantTree();
+                success = ControllerWorldState.getInstance().plantTree();
                 break;
 
             case GET_WORMS:
-                ControllerWorldState.getInstance().getWorms();
+                success = ControllerWorldState.getInstance().getWorms();
                 break;
 
             case FILL_CAT_DISH:
-                ControllerWorldState.getInstance().fillCatDish();
+                success = ControllerWorldState.getInstance().fillCatDish();
                 break;
             case BUY_CAT_FOOD:
-                ControllerWorldState.getInstance().buyCatFood();
+                success = ControllerWorldState.getInstance().buyCatFood();
                 break;
             case GET_CAT_FOOD:
-                ControllerWorldState.getInstance().getCatFood();
+                success = ControllerWorldState.getInstance().getCatFood();
                 break;
             case FIX_BENCH:
-                ControllerWorldState.getInstance().fixBench();
+                success = ControllerWorldState.getInstance().fixBench();
                 break;
             case CATCH_FISH:
-                ControllerWorldState.getInstance().catchFish();
+                success = ControllerWorldState.getInstance().catchFish();
                 break;
 
             case GO_TO_OUTDOOR:
+                needToShow = false;
                 ControllerMain.getInstance().navigateScreen(new ScreenGameOutdoor(172, 12));
                 break;
             case RETURN_TO_OUTDOOR:
+                needToShow = false;
                 ControllerMain.getInstance().navigateScreen(new ScreenGameOutdoor(16, 12));
                 break;
             case GO_TO_HOME:
+                needToShow = false;
                 ControllerMain.getInstance().navigateScreen(new ScreenGameHome(204, 12));
                 break;
             case GO_TO_MARKET:
+                needToShow = false;
                 ControllerMain.getInstance().navigateScreen(new ScreenGameMarket(178, 12));
                 break;
             case GO_TO_PARK:
+                needToShow = false;
                 ControllerMain.getInstance().navigateScreen(new ScreenGamePark(16, 12));
                 break;
             case RETURN_TO_PARK_FROM_MARKET:
+                needToShow = false;
                 ControllerMain.getInstance().navigateScreen(new ScreenGamePark(124, 12));
                 break;
             case GO_TO_FOREST:
+                needToShow = false;
                 ControllerMain.getInstance().navigateScreen(new ScreenGameForest(16, 12));
                 break;
             case RETURN_TO_PARK_FROM_FOREST:
+                needToShow = false;
                 ControllerMain.getInstance().navigateScreen(new ScreenGamePark(690, 12));
                 break;
         }
+
+        if (needToShow)
+            ControllerWorldState.getInstance().setMessageDialog(success ? itemInteracted.getMessageSuccess() : itemInteracted.getMessageFault());
+        wLocationInfo.resetInteractedItem();
     }
 }
