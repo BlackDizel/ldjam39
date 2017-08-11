37d4229de5f5d8d81b07ef431462718d59ab26c7
blackdizel <blackdizel@gmail.com>
2017-08-11 21:47:18 +0300

    implement mobile input

--- core/src/org/byters/ldjam39/view/input/InputEnum.java
+++ core/src/org/byters/ldjam39/view/input/InputEnum.java
@@ -3,18 +3,19 @@ package org.byters.ldjam39.view.input;
 import com.badlogic.gdx.Input;
 
 public enum InputEnum {
-    KEY_RUN(Input.Keys.SHIFT_LEFT),
     KEY_MOVE_LEFT(Input.Keys.A),
-    KEY_MOVE_UP(Input.Keys.W),
     KEY_MOVE_RIGHT(Input.Keys.D),
-    KEY_MOVE_DOWN(Input.Keys.S),
-    KEY_SHOW_MOBILE(Input.Keys.SPACE),
     KEY_CANCEL(Input.Keys.ESCAPE),
     KEY_INTERACT(Input.Keys.E),
+    KEY_CONFIRM(Input.Keys.ENTER),
+
     KEY_MOBILE_NEXT(Input.Keys.RIGHT),
     KEY_MOBILE_PREV(Input.Keys.LEFT),
-    KEY_MOBILE_PHONEBOOK(Input.Keys.NUM_3),
-    KEY_CONFIRM(Input.Keys.ENTER);
+    KEY_MOBILE_PHONEBOOK(Input.Keys.NUM_2),
+    KEY_MOBILE_TASKS(Input.Keys.NUM_1),
+    KEY_MOBILE_NEXT_ITEM(Input.Keys.DOWN),
+    KEY_MOBILE_PREV_ITEM(Input.Keys.UP),
+    KEY_SHOW_MOBILE(Input.Keys.SPACE);
 
     private final int key;
 
--- core/src/org/byters/ldjam39/view/input/InputMobile.java
+++ core/src/org/byters/ldjam39/view/input/InputMobile.java
@@ -2,13 +2,16 @@ package org.byters.ldjam39.view.input;
 
 
 import com.badlogic.gdx.Gdx;
+import org.byters.ldjam39.controller.InteractionMobile;
 import org.byters.ldjam39.model.Mobile;
 
 public class InputMobile {
+    private final InteractionMobile interactionMobile;
     private Mobile wMobile;
 
-    public InputMobile(Mobile mobile) {
+    public InputMobile(Mobile mobile, InteractionMobile interactionMobile) {
         this.wMobile = mobile;
+        this.interactionMobile = interactionMobile;
     }
 
     public void input() {
@@ -26,13 +29,26 @@ public class InputMobile {
         if (wMobile == null || !wMobile.isShown())
             return;
 
-        /*if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_NEXT.getKey()))
-            wMobile.get().nextScreen();
+        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_NEXT.getKey()))
+            wMobile.nextScreen();
 
         if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_PREV.getKey()))
-            wMobile.get().prevScreen();
+            wMobile.prevScreen();
+
+        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_NEXT_ITEM.getKey()))
+            wMobile.nextItem();
+
+        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_PREV_ITEM.getKey()))
+            wMobile.prevItem();
+
+        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_INTERACT.getKey())) {
+            interactionMobile.interact();
+        }
 
         if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_PHONEBOOK.getKey()))
-            wMobile.get().openClosePhonebook();*/
+            wMobile.openClosePhonebook();
+
+        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_TASKS.getKey()))
+            wMobile.openTasks();
     }
 }
