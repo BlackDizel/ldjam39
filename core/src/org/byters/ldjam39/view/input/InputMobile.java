c34b4b18db8a5ab6417e40570f4ad08c5ee63127
blackdizel <blackdizel@gmail.com>
2017-08-20 10:39:33 +0300

    implement mobile input return boolean

--- core/src/org/byters/ldjam39/view/input/InputMobile.java
+++ core/src/org/byters/ldjam39/view/input/InputMobile.java
@@ -14,41 +14,59 @@ public class InputMobile {
         this.interactionMobile = interactionMobile;
     }
 
-    public void input() {
-        if (wMobile == null) return;
+    public boolean input() {
+        if (wMobile == null) return false;
 
-        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_SHOW_MOBILE.getKey()))
+        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_SHOW_MOBILE.getKey())) {
             wMobile.setShown(true);
-        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_CANCEL.getKey()) && wMobile.isShown())
+            return true;
+        }
+        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_CANCEL.getKey()) && wMobile.isShown()) {
             wMobile.setShown(false);
+            return true;
+        }
 
-        inputMobileShown();
+        return inputMobileShown();
     }
 
-    private void inputMobileShown() {
+    private boolean inputMobileShown() {
         if (wMobile == null || !wMobile.isShown())
-            return;
+            return false;
 
-        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_NEXT.getKey()))
+        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_NEXT.getKey())) {
             wMobile.nextScreen();
+            return true;
+        }
 
-        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_PREV.getKey()))
+        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_PREV.getKey())) {
             wMobile.prevScreen();
+            return true;
+        }
 
-        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_NEXT_ITEM.getKey()))
+        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_NEXT_ITEM.getKey())) {
             wMobile.nextItem();
+            return true;
+        }
 
-        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_PREV_ITEM.getKey()))
+        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_PREV_ITEM.getKey())) {
             wMobile.prevItem();
+            return true;
+        }
 
         if (Gdx.input.isKeyJustPressed(InputEnum.KEY_INTERACT.getKey())) {
             interactionMobile.interact();
+            return true;
         }
 
-        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_PHONEBOOK.getKey()))
+        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_PHONEBOOK.getKey())) {
             wMobile.openClosePhonebook();
+            return true;
+        }
 
-        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_TASKS.getKey()))
+        if (Gdx.input.isKeyJustPressed(InputEnum.KEY_MOBILE_TASKS.getKey())) {
             wMobile.openTasks();
+            return true;
+        }
+        return false;
     }
 }
